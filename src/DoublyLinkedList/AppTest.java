9480ad7251c9a5914d2d2d0cd981a9a5125f45ce
Saliheddine Oussama <oussam.salih@outlook.fr>
2019-04-15 12:23:11 +0100

    adding doubly linked list

--- src/DoublyLinkedList/AppTest.java
+++ src/DoublyLinkedList/AppTest.java
@@ -3,7 +3,24 @@ package DoublyLinkedList;
 public class AppTest {
 
 	public static void main(String[] args) {
-
+		List<Integer> myDoublyList = new DoublyLinkedList<Integer>();
+		
+		myDoublyList.add(3);
+		myDoublyList.add(45);
+		myDoublyList.add(-43);
+		myDoublyList.add(289);
+		System.out.println("The number of elements in my doubly linked list is : " + myDoublyList.countElement());
+		
+		myDoublyList.printList();
+		myDoublyList.remove(45);
+		myDoublyList.printList();
+		myDoublyList.remove(289);
+		myDoublyList.printList();
+		myDoublyList.remove(-43);
+		myDoublyList.printList();
+		myDoublyList.remove(3);
+		myDoublyList.printList();
+		System.out.println("The number of elements in my doubly linked list after removing all items is : " + myDoublyList.countElement());
 	}
 
 }
--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -1,9 +1,61 @@
 package DoublyLinkedList;
 
-public class DoublyLinkedList<T extends Comparable<T>> {
+public class DoublyLinkedList<T extends Comparable<T>> implements List<T>{
 	
 	private Node<T> root;
 	private int counter;
+	
+	@Override
+	public void add(T data) {
+		++this.counter;
+		if(this.root == null) {
+			this.root = new Node<T>(data);
+			this.root.setPrevious(null);
+			this.root.setNext(null);
+		}
+		else{
+			Node<T> newNode = new Node<T>(data);
+			newNode.setNext(this.root);
+			this.root.setPrevious(newNode);
+			this.root = newNode;
+		}
+	}
+	@Override
+	public void remove(T data) {
+		if(this.root == null) return;
+		if(this.root.getData().compareTo(data) == 0){
+			--this.counter;
+			this.root = this.root.getNext();
+		}
+		else{
+			--this.counter;
+			removeNode(data, this.root);
+		}
+	}
+	private void removeNode(T data, Node<T> node) {
+		while(node != null){
+			if(node.getData().compareTo(data) == 0){
+					node.getPrevious().setNext(node.getNext());
+					node.getNext().setPrevious(node.getPrevious());	
+					return;
+			}
+			node = node.getNext();
+		}
+	}
+	@Override
+	public void printList() {
+		Node<T> nodes = this.root;
+		while(nodes != null){
+			System.out.print(nodes + " <-> ");
+			nodes = nodes.getNext();
+		}
+		System.out.print("null");
+		System.out.println();
+	}
+	@Override
+	public int countElement() {
+		return this.counter;
+	}
 	
 	
 
